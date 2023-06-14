91d840d7d8ac3d5d420a0cf08ec4b7fc6640ce40
Jawascript <n.jmonplaisir@gmail.com>
2023-06-14 13:52:37 -0400

    first commit | bare file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeeDirectory from './components/EmployeeDirectory';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeDirectory />
+    </div>
+  );
+}
+
+export default App;
