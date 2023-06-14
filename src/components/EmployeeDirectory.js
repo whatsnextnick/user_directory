99f99efc1a384bf3d2e85c0a7ac8ab0f9d89220d
Jawascript <n.jmonplaisir@gmail.com>
2023-06-14 15:13:34 -0400

    added prepopulated employee data | replaced 'employees' with 'employeeData' and added dropdown menu

--- src/components/EmployeeDirectory.js
+++ src/components/EmployeeDirectory.js
@@ -1,21 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import EmployeeTable from './EmployeeTable';
+import employees from '../data/employees'; // Import the employee data
 
 const EmployeeDirectory = () => {
-  const [employees, setEmployees] = useState([]);
+  const [employeeData, setEmployeeData] = useState([]);
+  const [filterOption, setFilterOption] = useState('name');
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
   useEffect(() => {
-    // Fetch employee data from an API or local file
-    fetch('https://api.example.com/employees')
-      .then((response) => response.json())
-      .then((data) => setEmployees(data))
-      .catch((error) => console.log(error));
+    setEmployeeData(employees);
+    setFilteredEmployees(employees);
   }, []);
+
+  const handleFilterChange = (event) => {
+    setFilterOption(event.target.value);
+    filterEmployees(event.target.value);
+  };
+
+  const filterEmployees = (filterOption) => {
+    if (filterOption === 'name') {
+      // Filter employees by name
+      const filtered = employeeData.filter((employee) =>
+        employee.name.toLowerCase().includes(filterOption.toLowerCase())
+      );
+      setFilteredEmployees(filtered);
+    }
+  };
 
   return (
     <div>
       <h1>Employee Directory</h1>
-      <EmployeeTable employees={employees} />
+      <div>
+        <label htmlFor="filterOption">Filter by:</label>
+        <select id="filterOption" value={filterOption} onChange={handleFilterChange}>
+          <option value="name">Name</option>
+          {/* Add more filter options if needed */}
+        </select>
+      </div>
+      <EmployeeTable employees={filteredEmployees} />
     </div>
   );
 };
---
+++ src/data/employee.js
@@ -0,0 +1,23 @@
+const employees = [
+    {
+      id: 1,
+      name: 'Jean Pierre',
+      department: 'Sales',
+      position: 'Sales Manager',
+    },
+    {
+      id: 2,
+      name: 'Sophie Joseph',
+      department: 'Marketing',
+      position: 'Marketing Specialist',
+    },
+    {
+      id: 3,
+      name: 'Mireille Dupont',
+      department: 'IT',
+      position: 'Software Engineer',
+    },
+  ];
+  
+  export default employees;
+  
